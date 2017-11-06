f5848424b260366b158223f3bb2e723c4b59d06a
yuyaebean <yuyaebean@naver.com>
2017-11-06 23:52:52 +0900

    Make about & contact page / overall update

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.render('index', { title: 'For P' });
+});
+
+router.get('/about', (req, res, next) => {
+  res.render('about');
+});
+
+router.get('/contact', (req, res, next) => {
+  res.render('contact');
 });
 
 module.exports = router;

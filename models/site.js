aa325422f80fd76fd058440f3aeb3dc435171f87
Daham Lee <dlek1011@gmail.com>
2017-12-04 20:47:13 +0900

    signin, signout

--- models/site.js
+++ models/site.js
@@ -3,12 +3,16 @@ var mongoosePaginate = require('mongoose-paginate');
 var Schema = mongoose.Schema;
 
 var schema = new Schema({
+  author: {type: Schema.Types.ObjectId, ref: 'User'},
   name: {type: String, unique: true, required: true},
   domain: {type: String, trim: true, required: true},
   includeLang: {type: [String], required: true},
   language: {type: [String], required: true},
   feature: {type: [String], required: true},
   created: {type: Date, default: Date.now}
+},{
+  toJSON: {virtuals: true},
+  toObject: {virtuals: true}
 });
 
 schema.plugin(mongoosePaginate);
